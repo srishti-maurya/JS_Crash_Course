f827a52fd1e276385cd394d6619d7c63ece032ef
srishti-maurya <srishti22maurya@gmail.com>
2021-11-22 14:44:58 +0530

    Adding user

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
-const btn = document.querySelector(".btn");
+const myForm = document.querySelector("#my-form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("#email");
+const msg = document.querySelector(".msg");
+const userList = document.querySelector("#users");
 
-// Mouse Event
-btn.addEventListener("click", (e) => {
+myForm.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
   e.preventDefault();
-  console.log(e.target.className);
-  document.getElementById("my-form").style.background = "#ccc";
-  document.querySelector("body").classList.add("bg-dark");
-  document.querySelector(".items").lastElementChild.innerHTML =
-    "<h1>Changed</h1>";
-});
+
+  if (nameInput.value === "" || emailInput.value === "") {
+    msg.classList.add("error");
+    msg.innerHTML = "Please enter all fields";
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    const li = document.createElement("li");
+    li.appendChild(
+      document.createTextNode(`${nameInput.value}: ${emailInput.value}`)
+    );
+    userList.appendChild(li);
+    nameInput.value = "";
+    emailInput.value = "";
+  }
+}
